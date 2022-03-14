0b97b9335abce8019c197b4baa92b6f80e0409ef
Rhi <codingmaniac796@gmail.com>
2022-03-14 16:10:23 -0600

    saving and loading should be a thing now

--- clicker.js
+++ clicker.js
@@ -2,11 +2,11 @@
 var winWidth;
 var prevWinWidth;
 
+//some clicker stuff
 var clicks = 0;
 var CPS = 0;
 var CPC = 1;
 var purchasesMenuClassint = 1;
-var doSave = 1;
 
 var cursor = {
     Price: 15,
@@ -38,6 +38,16 @@ var clickerUp = {
     Own: 1,
 }
 
+
+//load from local storage, if there are variables saved (this needs to be AFTER variable declaration)
+var doLoad = localStorage.getItem("doLoad")
+if (doLoad != null) {
+    loadItem(cursor, "cursor");
+    loadItem(cursor2, "cursor2");
+    loadItem(clicker, "clicker");
+    loadItem(clickerUp, "clickerUp");
+}
+
 //Function for purchasing buttons
 //If no adding to a variable, (eg, CPS) is required, just set addto and addamount to 0
 var purchase = {
@@ -61,6 +71,16 @@ var purchase = {
     },
 }
 
+function saveItem(item, id) {
+    localStorage.setItem(id + "price", item.Price);
+    localStorage.setItem(id + "own", item.Own);
+}
+
+function loadItem(item, id) {
+    item.Price = +localStorage.getItem(id + "price");
+    item.Own = +localStorage.getItem(id + "own");
+}
+
 $(document).ready(function() {
     function textUpdate () {
         $("#clicks").html("Clicks: " + clicks);
@@ -82,6 +102,13 @@ $(document).ready(function() {
 
     function second() {
         clicks += CPS;
+
+        //Save Items
+        saveItem(cursor, "cursor");
+        saveItem(cursor2, "cursor2");
+        saveItem(clicker, "clicker");
+        saveItem(clickerUp, "clickerUp");
+        localStorage.setItem("doLoad", "dummy");
     }
     setInterval(second, 1000);
 });
