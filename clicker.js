7367fc81ca89bb77c260a12eaf8d56337d92c928
Rhi <codingmaniac796@gmail.com>
2022-03-14 16:51:42 -0600

    that was a lot of code hopefully it all works

--- clicker.js
+++ clicker.js
@@ -23,18 +23,30 @@ var cursor2 = {
     Own: 0,
     priceUp: 500,
     addVal: 10,
+    updateElements: function() {
+        $('#cursorPrice2').html("Cost: " + this.Price);
+        $('#cursorOwn2').html("Owned: " + this.Own);
+    }
 }
 
 var clicker = {
     Price: 100,
     priceAdd: 50,
     Own: 0,
+    updateElements: function() {
+        $('#clickerPrice').html("Cost: " + this.Price);
+        $('#clickerOwn').html("Owned: " + this.Own);
+    }
 }
 
 var clickerUp = {
     Price: 1000,
     priceAdd: 500,
     Own: 1,
+    updateElements: function() {
+        $('#clickerUpPrice').html("Cost: " + this.Price);
+        $('#clickerUpOwn').html("Owned: " + (this.Own-1));
+    }
 }
 
 
@@ -53,11 +65,15 @@ if (doLoad != null) {
 //Function for purchasing buttons
 //If no adding to a variable, (eg, CPS) is required, just set addto and addamount to 0
 var purchase = {
-    noAdd: function(thing) {
+    noAdd: function(thing, expo) {
         if (clicks >= thing.Price) {
             thing.Own += 1;
             clicks -= thing.Price;
-            thing.Price += thing.priceAdd;
+            if (expo) {
+                thing.price = thing.price * 2;
+            } else {
+                thing.Price += thing.priceAdd;
+            }
         }
     },
     add: {
@@ -89,6 +105,8 @@ $(document).ready(function() {
         $("#clicks").html("Clicks: " + clicks);
         $("#cps").html("Clicks Per Second: " + CPS);
         $("#cpc").html("Clicks Per Click: " + CPC);
+
+        CPS = clicker.Own * clickerUp.Own;
 
         //code for detecting window changes
         winWidth = $(window).width();
